f5648c864aad586aec38ff20e30a94273032f0ce
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2023-05-17 21:14:05 +0200

    Update App.tsx
    Added parentheses to return App

--- move-up-fitness/src/App.tsx
+++ move-up-fitness/src/App.tsx
@@ -1,9 +1,11 @@
 import Navbar from "@/scenes/navbar";
 
 function App() {
-	return <div className="App bg-gray-20">
-		<Navbar />
-		</div>;
+	return (
+		<div className="app bg-gray-20">
+			<Navbar />
+		</div>
+	);
 }
 
 export default App;
