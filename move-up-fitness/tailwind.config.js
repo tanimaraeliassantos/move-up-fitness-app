b40a7c1bba13a74da1eda5b83c71bdca206545e7
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2023-05-17 21:17:24 +0200

    Update tailwind.config.js

--- move-up-fitness/tailwind.config.js
+++ move-up-fitness/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
@@ -38,5 +38,5 @@ export default {
     }
   },
   plugins: [],
-}
+};
 
