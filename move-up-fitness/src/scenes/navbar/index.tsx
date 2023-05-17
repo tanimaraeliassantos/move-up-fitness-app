6f03dcb8cf435149bbd1cf34840ee2b9b449cb77
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2023-05-17 21:36:13 +0200

    Update index.tsx
    Added spacing to the div

--- move-up-fitness/src/scenes/navbar/index.tsx
+++ move-up-fitness/src/scenes/navbar/index.tsx
@@ -26,7 +26,7 @@ const Navbar = (props: Props) => {
                         <p>Our Classes</p>
                         <p>Contact Us</p>
                     </div>
-                    <div>
+                    <div className={`${flexBetween} gap-8`}>
                         <p>Sign In</p>
                         <button>Become a member</button>
                     </div>
