4e8fb1a3a92126b6c877f8460d7e5fde9782b21b
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2023-06-12 12:48:27 +0200

    Update App.tsx
    Set Enum to restrict type of elements from navigation bar

--- move-up-fitness/src/App.tsx
+++ move-up-fitness/src/App.tsx
@@ -1,8 +1,15 @@
 import Navbar from "@/scenes/navbar";
 import { useState } from "react";
 
+ enum SelectedPage {
+	Home = "home",
+	Benefits = "benefits",
+	OurClasses = "ourclasses",
+	ContactUs = "contactus"
+ }
+
 function App() {
-	const [selectedPage, setSelectedPage] = useState<string>('home');
+	const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
 	return (
 		<div className="app bg-gray-20">
 			<Navbar 
