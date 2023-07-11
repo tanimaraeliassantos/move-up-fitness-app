16e85864937d81c5b3370cae6e8052caeb25db32
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2023-07-11 13:26:57 +0200

    Update index.tsx
    Added anchored button component to become a member

--- move-up-fitness/src/scenes/navbar/index.tsx
+++ move-up-fitness/src/scenes/navbar/index.tsx
@@ -4,6 +4,7 @@ import Logo from "@/assets/Logo.png";
 import Link from "./Link";
 import { SelectedPage } from "@/shared/types";
 import useMediaQuery from "@/hooks/useMediaQuery";
+import ActionButton from "@/shared/ActionButton";
 
 type Props = {
     selectedPage: SelectedPage;
@@ -52,7 +53,7 @@ const Navbar = ({selectedPage, setSelectedPage}: Props) => {
                     </div>
                     <div className={`${flexBetween} gap-8`}>
                         <p>Sign In</p>
-                        <button>Become a member</button>
+                        <ActionButton>Become a member</ActionButton>
                     </div>
                 </div>
                 ) : (
