3adb7afadd3d736afba16d0b2dc12928e4a088f2
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2023-07-11 12:22:35 +0200

    Update index.tsx
    Added hook useMediaQuery created previously by creating a boolean constant that will tell us if it's above or below the width we set here

--- move-up-fitness/src/scenes/navbar/index.tsx
+++ move-up-fitness/src/scenes/navbar/index.tsx
@@ -3,6 +3,7 @@ import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import Logo from "@/assets/Logo.png";
 import Link from "./Link";
 import { SelectedPage } from "@/shared/types";
+import useMediaQuery from "@/hooks/useMediaQuery";
 
 type Props = {
     selectedPage: SelectedPage;
@@ -11,6 +12,7 @@ type Props = {
 
 const Navbar = ({selectedPage, setSelectedPage}: Props) => {
     const flexBetween = "flex items-center justify-between";
+    const isAboveMediumScreens = useMediaQuery("(min-width: 1060px");
   return (
     <nav>
         <div
