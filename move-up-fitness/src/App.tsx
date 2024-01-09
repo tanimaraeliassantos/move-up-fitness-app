80cecfffe058c5a18b847ac296c36090fcb35efd
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2024-01-09 12:43:38 +0100

    Update App.tsx
    Handled scroll with a useEffect function. If the app is at the top of the page, then Home is the enum member selected

--- move-up-fitness/src/App.tsx
+++ move-up-fitness/src/App.tsx
@@ -1,12 +1,20 @@
 import Navbar from "@/scenes/navbar";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { SelectedPage } from "@/shared/types";
 
 function App() {
 	const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
 	const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
 
-	
+	useEffect(() => {
+		const handleScroll = () => {
+			if(window.scrollY === 0) {
+				setIsTopOfPage(true);
+				setSelectedPage(SelectedPage.Home);
+			}
+			if(window.scrollY !== 0) setIsTopOfPage(false);
+		}
+	})
 	return (
 		<div className="app bg-gray-20">
 			<Navbar 
