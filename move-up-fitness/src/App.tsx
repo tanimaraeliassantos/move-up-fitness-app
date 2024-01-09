1e756f548a0d2d930e8315a6155341e096bb22f6
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2024-01-09 12:39:18 +0100

    Update App.tsx
    Created type for fixed nav bar when scrolling through the app

--- move-up-fitness/src/App.tsx
+++ move-up-fitness/src/App.tsx
@@ -4,6 +4,9 @@ import { SelectedPage } from "@/shared/types";
 
 function App() {
 	const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
+	const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+	
 	return (
 		<div className="app bg-gray-20">
 			<Navbar 
